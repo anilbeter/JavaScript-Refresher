eed29f27af52e3fedd2ce30660417ad3ce221035
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-24 17:24:41 +0300

    add values and variables

--- Course Sections/01-Fundamentals-Part-1/starter/script.js
+++ Course Sections/01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+console.log('Hello World!');
+
+// Valuables and Variables
+
+console.log('Anil');
+console.log(23);
+
+let firstName = 'Anil';
+console.log(firstName);
+let lastName = 'Beter';
+console.log(lastName);
+
+let PI = 3.1415;
