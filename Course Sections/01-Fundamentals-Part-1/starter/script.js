44a2fb6b50b34f5a7dfcdec3bda3553c9fc8545a
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-24 22:31:54 +0300

    add data types

--- Course Sections/01-Fundamentals-Part-1/starter/script.js
+++ Course Sections/01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,38 @@
-console.log('Hello World!');
+// console.log('Hello World!');
 
 // Valuables and Variables
 
-console.log('Anil');
-console.log(23);
+// console.log('Anil');
+// console.log(23);
 
-let firstName = 'Anil';
-console.log(firstName);
-let lastName = 'Beter';
-console.log(lastName);
+// let firstName = 'Anil';
+// console.log(firstName);
+// let lastName = 'Beter';
+// console.log(lastName);
 
-let PI = 3.1415;
+// let PI = 3.1415;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+// boolean
+console.log(typeof javascriptIsFun);
+// boolean
+console.log(typeof 23);
+// number
+console.log(typeof 'Anil');
+// string
+
+javascriptIsFun = 'you right!';
+console.log(typeof javascriptIsFun);
+// string
+
+let anilJob;
+// undefined, i can change that variable later
+console.log(typeof anilJob);
+// undefined
+
+anilJob = 'Programmer!';
+console.log(anilJob);
+console.log(typeof anilJob);
