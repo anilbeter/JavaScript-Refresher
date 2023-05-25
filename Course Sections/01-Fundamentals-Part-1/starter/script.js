2b3b318e7299dbb8dcb00d3a0bc7b57d6a2bf012
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-25 08:12:07 +0300

    add challenge #2 (if and else statement)

--- Course Sections/01-Fundamentals-Part-1/starter/script.js
+++ Course Sections/01-Fundamentals-Part-1/starter/script.js
@@ -128,7 +128,7 @@ console.log(anilNew);
 console.log(`Kızgın duruyordun,
 Yüzünü dönüyordun,
 Dalgın  yürüyüşlerinin bir anlamı olmalı belki de?`);
-*/
+
 
 const age = 15;
 
@@ -146,3 +146,26 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century); // 20
+
+CHALLENGE #2
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+*/
+const johnHeight = 1.95;
+const johnWeight = 92;
+const markHeight = 1.69;
+const markWeight = 78;
+
+const johnBMI = johnWeight / johnHeight ** 2;
+const markBMI = markWeight / (markHeight * markHeight);
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
