a83ece194f32bd4dbbfb70f6f2be267603a5b082
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-26 09:53:21 +0300

    add challenge #4

--- Course Sections/01-Fundamentals-Part-1/starter/script.js
+++ Course Sections/01-Fundamentals-Part-1/starter/script.js
@@ -393,7 +393,7 @@ if (23 > 10) {
 // mesela templeta literal ın içine sadece expression koyabilirim, if else gibi statement koyamam
 console.log(`I'm ${2037 - 2000} years old.`) // valid sentence
 console.log(`${if (2 < 5)}`); // this gonna give error!!!
-*/
+
 
 const age = 23;
 age >= 18 ? console.log("I like to drink wine!") : console.log("You shouldn't drink alcohol!");
@@ -402,3 +402,26 @@ const drink = age >= 18 ? "Wine" : "Water"; // ternary operator is an expression
 console.log(drink);
 
 console.log(`I like to drink ${age >= 18 ? "Wine" : "Water"}`);
+
+
+// Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+*/
+const bill = 40;
+const tip = bill >= 50 && bill <= 300 ? bill * (15 / 100) : bill * (20 / 100);
+console.log(`Total bill: ${bill}, tip (accorde to the bill): ${tip}, total value: ${bill + tip}`);
