86fc5f980750131e4f350ba002b39544928c4a21
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-27 08:17:33 +0300

    add functions

--- Course Sections/02-Fundamentals-Part-2/starter/script.js
+++ Course Sections/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+function logger() {
+    console.log("My name is Anil");
+}
+
+// calling / running / function
+logger(); // My name is Anil
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(2, 4);
+// 2, 4
+
+const appleJuice = fruitProcessor(2, 0);
+console.log(appleJuice);
+// Juice with 2 apples and 0 oranges.
+console.log(fruitProcessor(2, 0));
+// Juice with 2 apples and 0 oranges.
+
+const appleOrangeJuices = fruitProcessor(2, 4);
+console.log(appleOrangeJuices);
+// Juice with 2 apples and 4 oranges.
