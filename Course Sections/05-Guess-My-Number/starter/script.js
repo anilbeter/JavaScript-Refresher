340ad55db6427a1d62c57fa13f8df68ae03b45ba
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-05-30 08:21:07 +0300

    selecting and manipulating events

--- Course Sections/05-Guess-My-Number/starter/script.js
+++ Course Sections/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+console.log(document.querySelector(".message").textContent);
+// Start guessing...
+
+document.querySelector(".message").textContent = "Anil burning up!";
+console.log(document.querySelector(".message").textContent);
+//Anil burning up!
+
+document.querySelector(".number").textContent = 23;
+document.querySelector(".score").textContent = 26;
+
+// .guess input'a ait olduğu için, inputtan veri alırken textContent yerine value kullanıyorum. Bu fark önemli!!!
+document.querySelector(".guess").value = 5;
