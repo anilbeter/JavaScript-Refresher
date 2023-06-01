cb8383949c7bb8a09a85b4dd0360cfc6c108e9fa
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-01 13:29:48 +0300

    add highscore implement

--- Course Sections/05-Guess-My-Number/starter/script.js
+++ Course Sections/05-Guess-My-Number/starter/script.js
@@ -18,6 +18,7 @@ document.querySelector(".guess").value = 5;
 
 let score = 20;
 let secretNum = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
 
@@ -30,6 +31,11 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector("body").style.backgroundColor = "#60b347";
 
         document.querySelector(".number").style.width = "30rem";
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = score;
+        }
+
     } else if (guess > secretNum) {
         if (score > 1) {
             document.querySelector(".message").textContent = "Too high.";
