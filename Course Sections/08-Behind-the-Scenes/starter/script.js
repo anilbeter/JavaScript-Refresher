1f8157a215dbaec5e4637c48d9fd8f8dc64e54f4
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-19 12:38:57 +0300

    scope in practice

--- Course Sections/08-Behind-the-Scenes/starter/script.js
+++ Course Sections/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    // You are 38, born in 1999
+
+    if (birthYear >= 1981 && birthYear <= 2001) {
+      var millenial = true;
+      const firstName = 'Sept';
+      // Oh, and you're a millenial, Sept
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      // Oh, and you're a millenial, Anil
+
+      function add(a, b) {
+        return a + b;
+      }
+      output = 'NEW OUTPUT';
+      console.log(output);
+    }
+    console.log(millenial); // true
+    // var oldugu icin block disindan da erisebiliyorum, let veya const olsaydi referenceError alacaktim!
+    // console.log(add(20, 3));
+    // referenceError #NOT: eğer "use strict" i silersem console'da 23 ü görebilirim, strict mode sayesinde functionları block scope olarak görebiliyorum (HER ZAMAN USE STRICT KULLAN)
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Anil';
+calcAge(1999);
+// console.log(age); // ReferenceError
+// printAge(); // ReferenceError
