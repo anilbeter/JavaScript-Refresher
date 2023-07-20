16add48673d05502ce7abafe2b183da96e4c7903
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-20 17:10:18 +0300

    destructring arrays

--- Course Sections/09-Data-Structures-Operators/starter/script.js
+++ Course Sections/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,48 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = [0];
+const b = [1];
+const c = [2];
+
+const [x, y, z] = arr;
+console.log(x, y, z); // 2 3 4
+
+const [first, second] = restaurant.categories;
+console.log(first, second); // Italian Pizzeria
+const [firstNew, , third] = restaurant.categories;
+console.log(firstNew, third); // Italian Vegetarian
+
+// Switching variables
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); // Pizzeria Italian
+
+// Same proccess with destructring
+[main, secondary] = [secondary, main];
+console.log(main, secondary); // Pizzeria Italian
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse); // Garlic Bread Pizza
+
+const nested = [2, 4, [5, 6]];
+// const [firstIndex, , thirdIndex] = nested;
+// console.log(firstIndex, thirdIndex); // 2 (2)[5,6]
+
+// Destructring inside destructring
+const [i, , [j, k]] = nested;
+console.log(i, j, k); // 2 5 6
+
+// Default values
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r); // 8 9 undefined
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); // 8 9 1
+// hepsini default olarak 1 yaptım, eğer kendim değer vermezsem 1 olarak gözükecek, undefined değil
