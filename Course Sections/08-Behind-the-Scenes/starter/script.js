6d3e4b58b15df3dfe212d6ea85ed7365731e16cb
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-20 09:18:39 +0300

    hoisting and TDZ practice

--- Course Sections/08-Behind-the-Scenes/starter/script.js
+++ Course Sections/08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -8,7 +9,7 @@ function calcAge(birthYear) {
     console.log(output);
     // You are 38, born in 1999
 
-    if (birthYear >= 1981 && birthYear <= 2001) {
+    if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
       const firstName = 'Sept';
       // Oh, and you're a millenial, Sept
@@ -36,3 +37,49 @@ const firstName = 'Anil';
 calcAge(1999);
 // console.log(age); // ReferenceError
 // printAge(); // ReferenceError
+
+*/
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Anil';
+var job = 'developer';
+const year = 1999;
+
+// Functions
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+// undefined
+if (!numProducts) deleteShoppingCart();
+// normal de bu if koşulunun yanlış olması gerekiyor ama var ile tanımladıgım icin şu an undefined (falsy), başında ! var true oldu ve koşul sağlandı.
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
