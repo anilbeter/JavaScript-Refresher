cdbf441269434b4250537bc0ca764944d680c46e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-23 11:01:08 +0300

    for-of loop

--- Course Sections/09-Data-Structures-Operators/starter/script.js
+++ Course Sections/09-Data-Structures-Operators/starter/script.js
@@ -55,23 +55,56 @@ const restaurant = {
     },
   },
 };
-// CHALLENGE
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+// Focaccia
+// Bruschetta
+// Garlic Bread
+// Caprese Salad
+// Pizza
+// Pasta
+// Risotto
+
+for (const [i, el] of menu.entries()) {
+  // console.log(item);
+  //  (2)[0, 'Focaccia']
+  //  (2)[1, 'Bruschetta']
+  //  (2)[2, 'Garlic Bread']
+  //  (2)[3, 'Caprese Salad']
+  //  (2)[4, 'Pizza']
+  //  (2)[5, 'Pasta']
+  //  (2)[6, 'Risotto']
+
+  console.log(`${i + 1}: ${el}`);
+  // 1: Focaccia
+  // 2: Bruschetta
+  // 3: Garlic Bread
+  // 4: Caprese Salad
+  // 5: Pizza
+  // 6: Pasta
+  // 7: Risotto
+}
+
 /*
-Coding Challenge #1
-We're building a football betting app (soccer for my American friends 😅)!
-Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
-Your tasks:
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic' 
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in) 
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-Then, call the function again with players from game.scored
-GOOD LUCK 😀
-*/
+// CHALLENGE
+
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic' 
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in) 
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK 😀
+
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -147,7 +180,7 @@ printGoals(...game.scored); // 4 goals were scored
 // 7
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
-/*
+
 
 /////////////////////////////////////////////////////
 const rest1 = {
