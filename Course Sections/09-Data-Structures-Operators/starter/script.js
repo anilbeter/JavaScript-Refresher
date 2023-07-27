a2386d3b48c302b4c8ea923a959e9d13a1eec294
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-27 09:26:20 +0300

    add string exercise

--- Course Sections/09-Data-Structures-Operators/starter/script.js
+++ Course Sections/09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -54,6 +50,24 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(
+    40
+  );
+  console.log(output);
+}
+
+/*
 
 const game = {
   team1: 'Bayern Munich',
@@ -95,7 +109,7 @@ const game = {
     team2: 6.5,
   },
 };
-// Challenge 3
+// Challenge 4
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
@@ -113,7 +127,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
-/*
+
 //////////////////////////////////////////////////
 // Working with String - Part 3
 
