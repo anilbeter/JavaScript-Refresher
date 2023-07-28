1fba2dd258e69b967b64d0e4e3e6dad096ab4874
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-28 10:29:53 +0300

    value vs. reference

--- Course Sections/10-Functions/starter/script.js
+++ Course Sections/10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -28,3 +29,35 @@ createBooking('KEK', 5);
 // {flightNum: 'KEK', numPassenger: 5, price: 995}
 createBooking('ADR23', undefined, 1000);
 // {flightNum: 'ADR23', numPassenger: 1, price: 1000}
+*/
+
+const flight = 'LH234';
+const anil = {
+  name: 'Anil Beter',
+  passport: 28489535,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 28489535) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, anil);
+console.log(flight);
+// LH234
+console.log(anil);
+// {name: 'Mr. Anil Beter', pasport: 28489535}
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(anil);
+checkIn(flight, anil);
+// Wrong passport!
