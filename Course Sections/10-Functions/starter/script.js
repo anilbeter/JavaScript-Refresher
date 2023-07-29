e7172bb181c7a5561c21c24ea722062d461740fc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-29 08:59:20 +0300

    add #challenge1 first task

--- Course Sections/10-Functions/starter/script.js
+++ Course Sections/10-Functions/starter/script.js
@@ -251,8 +251,7 @@ Your tasks:
     2: Rust
     3: C++
     (Write option number)
-  1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by
-1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+  1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 4. Run the 'displayResults' method at the end of each
@@ -264,4 +263,29 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    let answerFromUser = Number(
+      prompt(`What is your favourite programming language?
+    0: JavaScript
+    1: Python
+    2: Rust
+    3: C++
+    (Write option number)`)
+    );
+    if (
+      !(answerFromUser ??= 0) ||
+      answerFromUser === 1 ||
+      answerFromUser === 2 ||
+      answerFromUser === 3
+    ) {
+      this.answers[answerFromUser] = 1 + this.answers[answerFromUser];
+      console.log(this.answers);
+    } else {
+      console.log('Invalid Answer! Try Again!');
+    }
+    // this.answers[answerFromUser] = 1 + this.answers[answerFromUser];
+    // console.log(this.answers);
+  },
 };
+
+poll.registerNewAnswer();
