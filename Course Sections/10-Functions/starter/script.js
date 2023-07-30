ab8a843d72c3179639e6a4a609918670c328c258
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-30 14:25:42 +0300

    add challenge #2

--- Course Sections/10-Functions/starter/script.js
+++ Course Sections/10-Functions/starter/script.js
@@ -352,7 +352,7 @@ booker();
 // 2 passengers
 booker();
 // 3 passengers
-*/
+
 
 // Example 1
 let f;
@@ -394,3 +394,19 @@ const boardPassengers = function (n, wait) {
 // const perGroup = 1000;
 // eğer fonksiyon içinde bir perGroup variable'ı tanımlamasaydım otomatik olarak 1000 alacaktı, global scope'tan.
 boardPassengers(180, 3);
+
+///////////////////////////////////////
+Coding Challenge #2
+This is more of a thinking challenge than a coding challenge 🤓
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the body element is clicked. Do not select the h1 element again!
+2. And now explain to yourself (or someone around you) why this worked! Take all the time you need. Think about when exactly the callback function is executed, and what that means for the variables involved in this example.
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
