494a4ea4ba33fb463b74b987853dbdf372f608ff
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-02 11:51:23 +0300

    add challenge #2  (use map, filter and reduce methods)

--- Course Sections/11-Arrays-Bankist/starter/script.js
+++ Course Sections/11-Arrays-Bankist/starter/script.js
@@ -326,7 +326,7 @@ console.log(onlyDepositsFor);
 const onlyWithdrawals = movements.filter(mov => mov < 0);
 console.log(onlyWithdrawals);
 // (3) [-400, -650, -130]
-*/
+
 
 // Reduce method
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -383,3 +383,32 @@ console.log(max);
 // accumulator: 3000 and current movement: 70
 // accumulator: 3000 and current movement: 1300
 // 3000
+
+//////////////////////////////////////////////////////////////////////////////////////////
+Coding Challenge #2
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert
+dog ages to human ages and calculate the average age of the dogs in their study.
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+Test data:
+ Data 1: [5, 2, 4, 1, 15, 8, 3]
+ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function (ages) {
+  // map'teki array fonksiyonu doğru yazmadığım için bugı bulmakla uğraştım, next time daha dikkatli ol!!!
+  const humanAges = ages.map(age => (age <= 2 ? age * 2 : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log(avg1);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg2);
