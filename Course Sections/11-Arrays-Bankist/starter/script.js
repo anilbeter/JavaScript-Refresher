d650e736fe02e3819bf7d47fd31a71c8c0fcaf26
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-03 11:22:54 +0300

    add some method to our app

--- Course Sections/11-Arrays-Bankist/starter/script.js
+++ Course Sections/11-Arrays-Bankist/starter/script.js
@@ -183,6 +183,21 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 // Delete account
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
