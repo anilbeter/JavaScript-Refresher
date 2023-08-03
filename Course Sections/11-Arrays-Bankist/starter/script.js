cf19faacae0ea52d5fec93b5f1d85bfc27e6474c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-03 11:09:55 +0300

    add findIndex method (delete acc logic)

--- Course Sections/11-Arrays-Bankist/starter/script.js
+++ Course Sections/11-Arrays-Bankist/starter/script.js
@@ -183,6 +183,29 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// Delete account
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // delete account
+    accounts.splice(index, 1);
+
+    // hide UI
+    containerApp.style.opacity = 0;
+
+    // change welcome message
+    labelWelcome.textContent = 'Log in to get started';
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
