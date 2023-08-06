0c034aafc7895049e9ccd5851d1e038edfdab223
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-06 20:05:44 +0300

    introduction to operations with dates

--- Course Sections/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ Course Sections/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -487,3 +487,16 @@ future.setFullYear(2040);
 console.log(future);
 // Mon Nov 19 2040 15:23:00 GMT+0300 (GMT+03:00)
 */
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(Number(future)); // 2142246180000
+console.log(+future); // 2142246180000
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+// 1000 --> millisecond, 60 --> second, 60 --> minute, 24 --> hour
+// bu işlemden sonra günü hesaplıyor
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
+console.log(days1);
+// 10
