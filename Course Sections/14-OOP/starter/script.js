2a85f5c8826a4caac274954aaa6924c36c28a23a
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-19 10:19:01 +0300

    introduction constructor function and new keyword

--- Course Sections/14-OOP/starter/script.js
+++ Course Sections/14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Constructor function
+const Person = function (firstName, birthYear) {
+  console.log(this); // Person {}
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const anil = new Person('Anil', 1999);
+console.log(anil);
+// Person {firstName: 'Anil', birthYear: 1999}
+
+//  ==== STEPS ====
+// 1. New {} is created
+// 2. Function is called, this = {}
+// 3. {} linked to prototype
+// 4. Function automatically return {}
