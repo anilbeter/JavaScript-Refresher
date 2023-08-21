bfa98c6c54f583067796a26d40001f5886517813
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-21 15:04:14 +0300

    add challenge #1

--- Course Sections/14-OOP/starter/script.js
+++ Course Sections/14-OOP/starter/script.js
@@ -97,3 +97,39 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 // (6) [3, 6, 4, 2, 5, 1]
 // Görüldüğü gibi unique() diye bir array method oluşturdum tıpkı map gibi filter gibi.
+
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+// Test data:
+// Data car 1: 'BMW' going at 120 km/h
+// Data car 2: 'Mercedes' going at 95 km/h
+
+// 1)
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// 2)
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+// 3)
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+// 4)
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+bmw.accelerate();
+bmw.break();
+mercedes.accelerate();
+mercedes.break();
