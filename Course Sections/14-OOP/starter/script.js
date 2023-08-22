dbf3c896655abc2fffb311fb57150235c4d810bc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-22 12:23:25 +0300

    add challenge #3

--- Course Sections/14-OOP/starter/script.js
+++ Course Sections/14-OOP/starter/script.js
@@ -364,3 +364,44 @@ const mike = new Student('Mike', 2020, 'Computer Science');
 console.log(mike); // Student {firstName: 'Mike', birthYear: 2020, course: 'Computer Science'}
 mike.introduce(); // My name is Mike and I study Computer Science
 mike.calcAge(); // 6
+
+///////////////////////////////////////////
+// Coding Challenge #3
+// Your tasks:
+// 1. Use a constructor function to implement an Electric Car (called 'EV') as a child "class" of 'Car'. Besides a make and current speed, the 'EV' also has the current battery charge in % ('charge' property)
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo'
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%'
+// 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! Hint: Review the definiton of polymorphism 😉
+// Test data:
+//  Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed, charge);
+  this.charge = charge;
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed}, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate();
