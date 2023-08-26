5fbc0cf82866c7fa42db16db53164a8fd62dd90a
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-26 09:11:23 +0300

    add challenge #4

--- Course Sections/14-OOP/starter/script.js
+++ Course Sections/14-OOP/starter/script.js
@@ -506,7 +506,7 @@ anil.introduce();
 // My name is Anil and I study Game Development
 anil.calcAge();
 // 27
-*/
+
 
 // 1) Public fields
 // 2) Private fields
@@ -601,3 +601,69 @@ Account.helper();
 acc1.deposit(300).deposit(120).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
 // (8) [250, -140, 1000, 300, 120, -35, 25000, -4000]
+
+////////////////////////////////////////////////
+////////////////////////////
+ Coding Challenge #4
+ Your tasks:
+ 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+ 2. Make the 'charge' property private
+ 3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. Then experiment with chaining!
+ Test data:
+  Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+ GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed}, with a charge of ${this.#charge}%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+rivian.accelerate().accelerate().brake().chargeBattery(50).accelerate();
+
+// ayriyeten parent elementteki setter ve getter ları inheritliyebiliyo/kullanabiliyor
+console.log(rivian.speedUS);
+// 109.375;
