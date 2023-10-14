64929b044c31a96b5b5abe2c34c1c621866f2332
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-14 09:59:07 +0300

    using geolocation API

--- Course Sections/15-Mapty/starter/script.js
+++ Course Sections/15-Mapty/starter/script.js
@@ -10,3 +10,24 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// first function -> success callback
+// second function -> error callback
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude);
+    // 39.9336842
+    console.log(longitude);
+    // 32.7294199
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu`
+      // yaklaşık olarak konumumu gösteren bir link oluşturdum
+    );
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
+// If I press Allow, output will ->  GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1697266426350}
