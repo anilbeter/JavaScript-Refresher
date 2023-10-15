8c161c943ec7caf0dc04af199273b6ee1d28cf9e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-15 09:01:00 +0300

    managing workout data: creating classes

--- Course Sections/15-Mapty/starter/script.js
+++ Course Sections/15-Mapty/starter/script.js
@@ -13,6 +13,51 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-5); // şu anki anlık zamanın mili sn cinsinden son 5 rakamını alıyorum id olarak, unique olması icin
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // im min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevetionGain) {
+    super(coords, distance, duration);
+    this.elevetionGain = elevetionGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// TESTING
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycling1);
+
+///////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
