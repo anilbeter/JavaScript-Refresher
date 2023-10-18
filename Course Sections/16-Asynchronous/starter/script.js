babbe9d4616cb7b5920166ff6591f06f77f98cad
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-18 08:17:22 +0300

    nested callback functions (display neighbour country)

--- Course Sections/16-Asynchronous/starter/script.js
+++ Course Sections/16-Asynchronous/starter/script.js
@@ -5,19 +5,11 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data); //use this to study the data you want to use.
-
-    const languages = Object.values(data.languages);
-    const currencies = Object.values(data.currencies);
-    const html = `
-  <article class="country">
+const renderCountry = function (data, className = '') {
+  const languages = Object.values(data.languages);
+  const currencies = Object.values(data.currencies);
+  const html = `
+  <article class="country ${className}">
  <img class="country__img" src= "${data.flags.svg}">
  <div class="country__data">
    <h3 class="country__name"> ${data.name.official}</h3>
@@ -30,15 +22,46 @@ const getCountryData = function (country) {
  </div>
 </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data); //use this to study the data you want to use.
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country
+    const neighbour = data.borders;
+    console.log(neighbour); // (8) ['ARM', 'AZE', 'BGR', 'GEO', 'GRC', 'IRN', 'IRQ', 'SYR']
+    if (!neighbour) return;
+
+    neighbour.forEach(neighbour => {
+      let request2 = new XMLHttpRequest();
+      request2.open(
+        'GET',
+        `https://restcountries.com/v3.1/alpha/${neighbour}
+      `
+      );
+      request2.send();
+      request2.addEventListener('load', function () {
+        const [data2] = JSON.parse(this.responseText);
+        console.log(data2);
+
+        // render neighbour countries
+        renderCountry(data2, 'neighbour');
+      });
+    });
   });
 };
 
 // Sample countries whose details we want to display.
-getCountryData('japan');
-getCountryData('australia');
-getCountryData('india');
-getCountryData('usa');
-getCountryData('turkey');
-getCountryData('sweden');
+getCountryAndNeighbour('usa');
