a1cf843d44cf459c92d2469d38547e7a856d3d5e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-19 21:03:31 +0300

    throwing errors manually

--- Course Sections/16-Asynchronous/starter/script.js
+++ Course Sections/16-Asynchronous/starter/script.js
@@ -99,28 +99,73 @@ console.log(request); // Promise {<pending>}
 //     });
 // };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(response => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) return;
+
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data[0], 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err}🌠`);
+//       renderError(`Something went wrong ${err.message}. Try again!`); // .catch method must be in the end of the chaining! catch will be handle all rejected errors (only called promise is rejected)
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     }); // Attaches a callback that is invoked when the Promise is settled (fulfilled or rejected)
+// };
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    `Country not found)`
+  )
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
-      if (!neighbour) return;
 
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      if (!neighbour) {
+        throw new Error('No neighbour found!');
+      }
+
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(err => {
       console.error(`${err}🌠`);
-      renderError(`Something went wrong ${err.message}. Try again!`);
-      // .catch method must be in the end of the chaining! catch will be handle all rejected errors (only called promise is rejected)
-
-      // Attaches a callback that is invoked when the Promise is settled (fulfilled or rejected)
+      renderError(`Something went wrong ${err.message}. Try again!`); // .catch method must be in the end of the chaining! catch will be handle all rejected errors (only called promise is rejected)
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
-    });
+    }); // Attaches a callback that is invoked when the Promise is settled (fulfilled or rejected)
 };
 
 btn.addEventListener('click', function () {
