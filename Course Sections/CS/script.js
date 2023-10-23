553271963d8d0e610232f6fee21afce1c5ac443f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-23 18:00:51 +0300

    callback hell

--- Course Sections/CS/script.js
+++ Course Sections/CS/script.js
@@ -1,5 +1,33 @@
-console.log("Sending request");
-setTimeout(() => {
-  console.log("Here is your data from the server!");
-}, 3000);
-console.log("I AM AT THE END OF THE FILE!");
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "red";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "orange";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "yellow";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = "green";
+//         setTimeout(() => {
+//           document.body.style.backgroundColor = "blue";
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+// Sıralama -> red, orange, yellow, green, blue
+
+const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext();
+  }, delay);
+};
+
+// delayedColorChange("red", 2000);
+// delayedColorChange("blue", 1000);
+// önce blue oluyor sonra red
+
+delayedColorChange("red", 1000, () => {
+  delayedColorChange("orange", 1000, () => {
+    delayedColorChange("yellow", 1000);
+  });
+});
