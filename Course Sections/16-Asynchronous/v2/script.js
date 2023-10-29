9d6cef50f84f50180fd71b825f451a44ea661fa0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-29 17:14:59 +0300

    Promisifying the Geolocation API pt2.

--- Course Sections/16-Asynchronous/v2/script.js
+++ Course Sections/16-Asynchronous/v2/script.js
@@ -284,17 +284,26 @@ getPosition().then(pos => console.log(pos));
 // GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1698588000892}...
 
 // Challenge #1
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+      );
+    })
     .then(res => {
       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
       return res.json();
     })
     .then(data => {
       console.log(data);
-      console.log(`You are in ${data.city}, ${data.country}`);
+      console.log(`You are in ${data.city}, ${data.countryName}`);
 
-      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/name/${data.countryName}`
+      );
     })
     .then(res => {
       if (!res.ok) throw new Error(`Country not found (${res.status})`);
@@ -304,6 +313,6 @@ const whereAmI = function (lat, lng) {
     .then(data => renderCountry(data[0]))
     .catch(err => console.error(`${err.message} 💥`));
 };
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.474);
+
+btn.addEventListener('click', whereAmI);
+// You are in Ankara, Turkey
