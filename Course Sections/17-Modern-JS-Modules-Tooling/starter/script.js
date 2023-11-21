287050cf67a28761feae2d911a1cbe8877332184
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-21 21:44:49 +0300

    Top-Level await (ES2022)

--- Course Sections/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Course Sections/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -21,3 +21,32 @@ console.log('Importing module');
 import add from './shoppingCart.js';
 add('pizza', 2);
 // 2 pizza added to cart
+
+// TOP-LEVEL await (only available in modules)
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+// Promise {<pending>}
+// ---> Because, async function always return a promise!
+
+// Not very clean solution:
+// lastPost.then(data => console.log(data));
+// {title: 'at nam consequatur ea labore ea harum', text: 'cupiditate quo est a modi nesciunt soluta\nipsa vol…nam et distinctio eum\naccusamus ratione error aut'}
+
+// Instead of using regular resolve stuff (like then, catch) use top-level await
+// More clean solution:
+const lastPost2 = await lastPost;
+console.log(lastPost2);
+// {title: 'at nam consequatur ea labore ea harum', text: 'cupiditate quo est a modi nesciunt soluta\nipsa vol…nam et distinctio eum\naccusamus ratione error aut'}
