8afe0fa60912aefe77e3875759d40a2acb4d4971
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-25 10:23:35 +0300

    loading recipe from API

--- Course Sections/18-forkify/starter/dist/index.e37f48ea.js
+++ Course Sections/18-forkify/starter/dist/index.e37f48ea.js
@@ -582,8 +582,19 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzzz");
+        const data = await res.json();
+        console.log(res, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["kYpTN","aenu9"], "aenu9", "parcelRequire3a11")
 
