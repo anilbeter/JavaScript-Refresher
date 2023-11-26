fce459d004af33a02be878e77842ddc4e1524a2c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-26 10:33:15 +0300

    create timeout function

--- Course Sections/18-forkify/starter/src/js/helpers.js
+++ Course Sections/18-forkify/starter/src/js/helpers.js
@@ -1,8 +1,18 @@
 import { async } from 'regenerator-runtime';
+import { TIMEOUT_SECONDS } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
 
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(url);
+    // İlk önce hangisi yüklenirse o gözükecek (race), yani 10sn içinde promise'ı çözemezse hata vericek timeout oldu vs
+    const res = await Promise.race[(fetch(url), timeout(TIMEOUT_SECONDS))];
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
