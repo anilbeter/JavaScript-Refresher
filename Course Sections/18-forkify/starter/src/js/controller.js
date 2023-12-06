ce13003ab6da7548acc26160c3291d12d6c20455
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-06 18:52:52 +0300

    create generateMarkup method for pagiantionView.js

--- Course Sections/18-forkify/starter/src/js/controller.js
+++ Course Sections/18-forkify/starter/src/js/controller.js
@@ -3,6 +3,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -43,7 +44,10 @@ const controlSearchResults = async function () {
 
     // 3) Render results
     // resultsView.render(model.state.search.results);
-    resultsView.render(model.getSearchResultsPage(1));
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
--- Course Sections/18-forkify/starter/src/js/views/paginationView.js
+++ Course Sections/18-forkify/starter/src/js/views/paginationView.js
@@ -3,4 +3,18 @@ import View from './View.js';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    ); // Math.ceil() -> her zaman yukarı yuvarlar
+    console.log(numPages);
+
+    // Page 1, and there are other pages
+    // Page 1, and there are NO other pages
+    // Last page
+    // Other page
+  }
 }
+
+export default new PaginationView();
