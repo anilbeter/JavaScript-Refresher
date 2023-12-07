1468c680c585f612950b62ba72e07cf75465ae0f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-07 21:33:50 +0300

    create controlServings function

--- Course Sections/18-forkify/starter/src/js/controller.js
+++ Course Sections/18-forkify/starter/src/js/controller.js
@@ -61,6 +61,14 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
