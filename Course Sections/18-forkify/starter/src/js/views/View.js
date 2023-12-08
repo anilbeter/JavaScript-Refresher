492a35a6d0b3a29676801fd02ac038538d962687
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-08 19:27:21 +0300

    develop algorithm for updating  DOM

--- Course Sections/18-forkify/starter/src/js/views/View.js
+++ Course Sections/18-forkify/starter/src/js/views/View.js
@@ -21,8 +21,26 @@ export default class View {
 
     // Convert string to real DOM Node
     const newDOM = document.createRange().createContextualFragment(newMarkup);
-    const newElements = newDOM.querySelectorAll('*');
-    console.log(newElements);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
   }
 
   _clear() {
