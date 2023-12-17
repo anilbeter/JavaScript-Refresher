0da71b0536023cb55704b4e6c1e77ba813f60d7b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-17 18:38:21 +0300

    change method names for refactored code

--- Course Sections/18-forkify/starter/src/js/model.js
+++ Course Sections/18-forkify/starter/src/js/model.js
@@ -1,9 +1,10 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config';
-import { getJSON } from './helpers';
+// import { getJSON } from './helpers';
 import { RES_PER_PAGE } from './config';
 import { sendJSON } from './helpers';
 import { KEY } from './api';
+import { AJAX } from './helpers';
 
 export const state = {
   recipe: {},
@@ -33,7 +34,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some(bookmark => bookmark.id === id))
@@ -51,7 +52,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
-    const data = await getJSON(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}`);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -147,7 +148,7 @@ export const uploadRecipe = async function (newRecipe) {
       servings: +newRecipe.servings,
       ingredients,
     };
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmark(state.recipe);
   } catch (err) {
